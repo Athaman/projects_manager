b7ade068f378bfa1261966fa65ba195c447bf3f8
Keone Martin <keonemartin@gmail.com>
2025-02-23 21:26:03 +1100

    add a no projects selected display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import ProjectSideBar from '../components/ProjectSidebar';
 import NewProject from '../components/NewProject';
-
+import NoProjectSelected from '../components/NoProjectSelected';
 export default function Home() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
