986c978ac3667d3d2dd6f1394eebb71156c3d48e
Keone Martin <keonemartin@gmail.com>
2025-02-23 20:54:36 +1100

    add create project form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import ProjectSideBar from '../components/ProjectSidebar';
+import NewProject from '../components/NewProject';
 
 export default function Home() {
   return (
-    <main className="h-screen my-8">
-      <ProjectSideBar />;
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSideBar />
+      <NewProject />
     </main>
   );
 }
