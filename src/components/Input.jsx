05345c46344b999690379cee629646f60aa655cb
Keone Martin <keonemartin@gmail.com>
2025-02-27 21:58:12 +1100

    add ids to label and input for accessibility

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,14 +1,21 @@
+import React, { useId } from 'react';
+
 export default function Input({ textarea, label, type, value, onChange, ...props }) {
+  const generatedId = useId();
+  const inputId = props.id || generatedId;
+
   const classes =
     'w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600';
 
   return (
     <p className="flex flex-col gap-1 my-4">
-      <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+      <label htmlFor={inputId} className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
       {textarea ? (
-        <textarea className={classes} {...props} value={value} onChange={onChange} />
+        <textarea id={inputId} className={classes} {...props} value={value} onChange={onChange} />
       ) : (
-        <input className={classes} {...props} type={type} value={value} onChange={onChange} />
+        <input id={inputId} className={classes} {...props} type={type} value={value} onChange={onChange} />
       )}
     </p>
   );
